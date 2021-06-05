e9c71d5a794e8d9605aa85777fb92ad498ebc4a6
Nadia <nlnadialigia@gmail.com>
2021-06-04 21:31:17 -0300

    feat: add Challenge007.py

---
+++ challenges/challenge007.py
@@ -0,0 +1,17 @@
+color = '\33[1;35m'
+clear = '\33[0;0m'
+grade1 = float(input('Insira a primeira nota: '))
+grade2 = float(input('Insira a segunda nota: '))
+
+print(color)
+print('Minha Resolução: ', end='')
+print(clear)
+avarage = (grade1 + grade2)/2
+print('Suas notas são {} e {}. A média de suas notas é {}' .format(grade1, grade2, avarage))
+
+print(color)
+print('Resolução do Professor: ', end='')
+print(clear)
+avarage = (grade1 + grade2)/2
+print('A média entre {} e {} é {}' .format(grade1, grade2, avarage))
+
