f7b03e1c4b24145dddcfd1bc6de15e4ed537dc6f
Nadia <nlnadialigia@gmail.com>
2021-06-05 01:03:47 -0300

    feat: add Challenge 041

---
+++ challenges/world-02/challenge041.py
@@ -0,0 +1,45 @@
+from datetime import date
+
+color = '\33[1;35m'
+clear = '\33[0;0m'
+
+print(color)
+print('Minha Resolução: ', end='')
+print(clear)
+
+year = int(input('Qual o ano de nascimento? '))
+age = date.today().year - year
+
+print('\33[33m*\33[34m' * 10)
+if age < 9:
+    print('MIRIM')
+elif 9 <= age < 14:
+    print('INFANTIL')
+elif 14 <= age < 19:
+    print('JÚNIOR')
+elif 19 <= age < 25:
+    print('SÊNIOR')
+else:
+    print('MASTER')
+print('\33[33m*\33[m' * 10)
+
+print(color)
+print('Resolução do Professor: ', end='')
+print(clear)
+
+year = int(input('Ano de nascimento: '))
+age = date.today().year - year
+print('O atleta tem {} anos.'.format(age))
+
+print('\33[33m*\33[34m' * 30)
+if age <= 9:
+    print('Classificação: MIRIM')
+elif age <= 14:
+    print('Classificação: INFANTIL')
+elif age <= 19:
+    print('Classificação: JÚNIOR')
+elif age <= 25:
+    print('Classificação: SÊNIOR')
+else:
+    print('Classificação: MASTER')
+print('\33[33m*\33[m' * 30)
