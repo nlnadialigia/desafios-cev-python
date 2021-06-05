d02318fccb9fecfd9610d2edee0d7afd33dd73aa
Nadia <nlnadialigia@gmail.com>
2021-06-04 21:56:48 -0300

    feat: refactor Challenge014.py

--- challenges/challenge014.py
+++ challenges/challenge014.py
@@ -4,7 +4,6 @@ clear = '\33[0;0m'
 print(color)
 print('Resolução do Professor: ', end='')
 print(clear)
-days = int(input('Quantos dias alugados? '))
-km = float(input('Quantos km rodados: '))
-rentCar = (days * 60) + (km * 0.15)
-print('O total a pagar é R${:.2f} por {} dias e {}km rodados.' .format(rentCar, days, km))
+c = float(input('Informe a temperatura em °C: '))
+f = 9 * c / 5 + 32
+print('A temperatura de {}°C corresponde a {}°F!' .format(c, f))
