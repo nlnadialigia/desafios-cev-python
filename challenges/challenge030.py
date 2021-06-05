8c53c0fa121fe21e1b6e1e10ca0017e6d3235666
Nadia <nlnadialigia@gmail.com>
2021-06-04 23:52:17 -0300

    feat: add Challenge 030

---
+++ challenges/challenge030.py
@@ -0,0 +1,19 @@
+color = '\33[1;35m'
+clear = '\33[0;0m'
+num = int(input('Digite um número inteiro: '))
+
+print(color)
+print('Minha Resolução: ', end='')
+print(clear)
+if num % 2 == 0:
+    print('O número {} é PAR'.format(num))
+else:
+    print('O número {} é ÍMPAR'.format(num))
+
+print(color)
+print('Resolução do Professor: ', end='')
+print(clear)
+if num % 2 == 0:
+    print('O número {} é PAR'.format(num))
+else:
+    print('O número {} é ÍMPAR'.format(num))
