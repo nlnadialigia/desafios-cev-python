f538b96ff39c5c75e61f744d9ae5a82bbc4a7933
Nadia <nlnadialigia@gmail.com>
2021-06-04 22:22:21 -0300

    feat: add Challenge 019

---
+++ challenges/challenge019.py
@@ -0,0 +1,22 @@
+from random import choice
+color = '\33[1;35m'
+clear = '\33[0;0m'
+a = input('Digite o aluno 01: ')
+b = input('Digite o aluno 02: ')
+c = input('Digite o aluno 03: ')
+d = input('Digite o aluno 04: ')
+
+print(color)
+print('Minha Resolução: ', end='')
+print(clear)
+students = a, b, c, d
+student = choice(students)
+print('O aluno sorteado foi {}' .format(student))
+
+
+print(color)
+print('Resolução do Professor: ', end='')
+print(clear)
+students = [a, b, c, d]
+student = choice(students)
+print('O aluno sorteado foi {}' .format(student))
