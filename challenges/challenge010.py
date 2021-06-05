3b2de9e58d0986f920e18f4d3847582839143acb
Nadia <nlnadialigia@gmail.com>
2021-06-04 21:47:32 -0300

    feat: add Challenge010.py

---
+++ challenges/challenge010.py
@@ -0,0 +1,19 @@
+color = '\33[1;35m'
+clear = '\33[0;0m'
+n = float(input('Quanto dinheiro você tem na carteira? '))
+exchange = 3.27
+
+print(color)
+print('Minha Resolução: ', end='')
+print(clear)
+result = n / exchange
+print('Você tem R${:.2f}, que corresponde a US${:.2f}' .format(n, result))
+
+print(color)
+print('Resolução do Professor: ', end='')
+print(clear)
+real = float(input('Quanto dinheiro você tem na carteira? R$'))
+dollar = real / exchange
+print('Com R${:.2f} você pode comprar US${:.2f}' .format(real, dollar))
+
+
