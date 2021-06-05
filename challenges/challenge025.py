048411f2beedf0ea4acaf90b9e538c484a3c67a8
Nadia <nlnadialigia@gmail.com>
2021-06-04 23:10:50 -0300

    feat: add Challenge 025

---
+++ challenges/challenge025.py
@@ -0,0 +1,14 @@
+color = '\33[1;35m'
+clear = '\33[0;0m'
+
+print(color)
+print('Minha Resolução: ', end='')
+print(clear)
+name = str(input('Nome completo: '))
+print('Silva' in name)
+
+print(color)
+print('Resolução do Professor: ', end='')
+print(clear)
+name = str(input('Nome completo: '))
+print('Seu nome tem Silva? {}'.format('Silva' in name.title()))
