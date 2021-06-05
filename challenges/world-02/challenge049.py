bd5ea2acffb8e81dd9c66258ddcdc1205239024b
Nadia <nlnadialigia@gmail.com>
2021-06-05 20:15:40 -0300

    feat: add Challenge 049

---
+++ challenges/world-02/challenge049.py
@@ -0,0 +1,24 @@
+color = '\33[1;35m'
+clear = '\33[0;0m'
+
+print(color)
+print('Minha Resolução: ', end='')
+print(clear)
+num = int(input('Escolha um número para a tabuada: '))
+print('*' * 15)
+print(' TABUADA DO {} '.format(num))
+print('*' * 15)
+for c in range(1, 11):
+    print('{:3} x {:2} = {:2}'.format(c, num, (c * num)))
+print('*' * 15)
+
+print(color)
+print('Resolução do Professor: ', end='')
+print(clear)
+num = int(input('Escolha um número para a tabuada: '))
+print('*' * 15)
+print('\33[1;33m TABUADA DO {} \33[m'.format(num))
+print('*' * 15)
+for c in range(1, 11):
+    print('{:3} x {:2} = {:2}'.format(num, c, (c * num)))
+print('*' * 15)
