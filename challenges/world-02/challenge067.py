66960a5eaa898285752ac3baeab9ab5e04cb1201
Nadia <nlnadialigia@gmail.com>
2021-06-05 22:51:09 -0300

    feat: add Challenge 067

---
+++ challenges/world-02/challenge067.py
@@ -0,0 +1,30 @@
+color = '\33[1;35m'
+clear = '\33[0;0m'
+
+print(color)
+print('Minha Resolução: ', end='')
+print(clear)
+while True:
+    num = int(input('Quer ver a tabuada de qual valor? '))
+    if num < 0:
+        break
+    c = 1
+    while c <= 10:
+        print(f'{c} x {num} = {(c * num):>2}'.center(40))
+        c += 1
+    print('\33[1;34m*\33[m' * 40)
+print('PROGRAMA TABUADA ENCERRADO. VOLTE SEMPRE!')
+
+print(color)
+print('Resolução do Professor: ', end='')
+print(clear)
+while True:
+    num = int(input('Quer ver a tabuada de qual valor? '))
+    if num < 0:
+        break
+    for c in range(1, 11):
+        print(f'{num} x {(c):>2} = {(c * num):>2}'.center(40))
+        c += 1
+    print('\33[1;34m*\33[m' * 40)
+print('PROGRAMA TABUADA ENCERRADO. VOLTE SEMPRE!')
+
