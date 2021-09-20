3f80ccb39b238b0b9e890a117e2027e1a2590e4e
Nadia <nlnadialigia@gmail.com>
2021-09-19 22:48:31 -0300

    finish Challenge 097

---
+++ challenges/world-03/challenge097.py
@@ -0,0 +1,12 @@
+def escreva(txt):
+    size = len(txt) + 5
+    print('~' * size)
+    print(txt.center(size))
+    print('~' * size)
+
+
+escreva('Gustavo Guanabara')
+
+escreva('Curso de Python no Youtube')
+
+escreva('CeV')
