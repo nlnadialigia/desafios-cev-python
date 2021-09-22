7865416dcb8d2eb740153eabbee4580c0aecda90
Nadia <nlnadialigia@gmail.com>
2021-09-22 00:31:35 -0300

    update files 79

--- challenges/world-03/Readme.md
+++ challenges/world-03/Readme.md
@@ -52,6 +52,7 @@ lista = ('Lapis', 10, 'Caneta', 20)
 - Crie um programa onde o usuário possa digitar vários valores numéricos e cadastre-os em uma lista. 
 - Caso o número já exista lá dentro, ele não será adicionado. 
 - No final, serão exibidos todos os valores únicos digitados, em ordem crescente.
+- [Resolução do Professor](./challenge079P.py)
 
 ⚙️ [Desafio 80 - Lista ordenada sem repetições ⁉️](./challenge080.py)
 - Crie um programa onde o usuário possa digitar cinco valores numéricos e cadastre-os em uma lista, já na posição correta de inserção (sem usar o sort()). 
--- challenges/world-03/challenge079P.py
+++ challenges/world-03/challenge079P.py
@@ -2,21 +2,20 @@ color = '\33[1;35m'
 clear = '\33[0;0m'
 lista = list()
 
-print(color)
-print('Minha Resolução: ', end='')
-print(clear)
 n = ' '
+print(color)
+print('Resolução do Professor: ', end='')
+print(clear)
 while True:
-    choice = ' '
     n = int(input('Digite um valor: '))
-    if n in lista:
-        print('Valor duplicado! Não vou adicionar...')
-    else:
+    if n not in lista:
         lista.append(n)
         print('Valor adicionado com sucesso...')
-    while choice not in 'SN':
-        choice = str(input('Quer continuar? [S/N] ')).upper()
-    if choice == 'N':
+    else:
+        print('Valor duplicado! Não vou adicionar...')
+    choice = str(input('Quer continuar? [S/N] '))
+    if choice in 'Nn':
         break
 print('-=' * 40)
+lista.sort()
 print(f'Você digitou os valores {lista}')
